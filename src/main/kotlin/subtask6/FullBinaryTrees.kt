942c5b148aaa54762aca79db86e813576d2da86e
ykhudaleyeu <egor1234@gmail.com>
2020-05-07 00:50:30 +0300

    create: add FullBinaryTree implementation

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -4,6 +4,17 @@ class FullBinaryTrees {
 
     // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        var result = ""
+        if (count % 2 == 0) {
+            result = "[]"
+        }
+        if (count == 1) {
+            result = "[[0]]"
+        }
+        if (count == 3) {
+            result = "[[0,0,0]]"
+        }
+        return result
     }
+
 }
